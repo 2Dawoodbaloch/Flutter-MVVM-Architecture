6f6b55e836c2f5559f5af9afac8c96ab46e378ce
2Dawoodbaloch <dawoodbloch2003@gmail.com>
2026-03-01 03:01:11 +0500

    routes defined

---
+++ lib/utils/routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_architecture/utils/routes_name.dart';
+import 'package:flutter_mvvm_architecture/view/home_screen.dart';
+import 'package:flutter_mvvm_architecture/view/login_screen.dart';
+
+class Routes {
+
+  static Route<dynamic> generateRoute(RouteSettings setting){
+    switch(setting.name){
+      case RoutesName.login :
+        return MaterialPageRoute(builder: (BuildContext context) => LoginScreen());
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (BuildContext context) => HomeScreen());
+        default:
+          return MaterialPageRoute(builder: (_) {
+          return Scaffold(
+            body: Center(
+              child: Text("No route Defined"),
+            ),
+          );
+        }
+        );
+    }
+  }
+}
