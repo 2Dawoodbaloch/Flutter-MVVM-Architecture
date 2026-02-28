37e521cf68e41b74366305c16f371aebd546076f
2Dawoodbaloch <dawoodbloch2003@gmail.com>
2026-03-01 02:03:36 +0500

    get post api response added

--- lib/data/network/NetworkApiServices.dart
+++ lib/data/network/NetworkApiServices.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter_mvvm_architecture/data/app_exceptions.dart';
 import 'package:flutter_mvvm_architecture/data/network/BaseApiServices.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-class Networkapiservices extends BaseApiServices {
+class NetworkApiServices extends BaseApiServices {
   @override
   Future<dynamic> getGetApiResponse(String url) async {
-    dynamic responseJson;
+    dynamic responseJson; // Why dynamic? Because API response could be:Map,List,String,Error message
     try {
       final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
       responseJson = returnResponse(response);
@@ -16,13 +16,25 @@ class Networkapiservices extends BaseApiServices {
       throw FetchDataException("no Internet Connection");
     }
 
-    return responseJson;
+    return responseJson; // Returns processed API data back to:  Repository➡ ViewModel➡ UI
   }
 
   @override
-  Future<dynamic> getPostApiResponse(String url) {
-    // TODO: implement getPostApiResponse
-    throw UnimplementedError();
+  Future<dynamic> getPostApiResponse(String url,dynamic data) async {
+
+    dynamic responseJson;
+    try {
+      Response response = await post(
+          Uri.parse(url),
+          body: data
+      );
+
+      responseJson = returnResponse(response);
+    } on SocketException{
+      throw FetchDataException("no Internet Connection");
+    }
+
+    return responseJson;
   }
   
 }
